8705ffc37a8fb5551f5a0c94ed9bbf297d4ff90e
Miraz Al-Mamun <mirazalmamun@gmail.com>
2014-08-30 13:48:08 +1000

    Added the initial batch of file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RubystoreMirazalmamunCom::Application.config.secret_key_base = '7c0be3e5393f8643a7badf79de35ccb72fb0a76251a608d2dff41dd44702bc303009342109b15099203ddeed20cc0ae3a20ec1ce44534b27a1a2c2f6baa21a0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RubystoreMirazalmamunCom::Application.config.session_store :cookie_store, key: '_rubystore_mirazalmamun_com_session'
